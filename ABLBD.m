3c53607a70a7a1691eb449f1c783a43005ace779
sgoldenCS <30479202+sgoldenCS@users.noreply.github.com>
2019-09-11 13:58:48 -0400

    Major Update

--- ABLBD.m
+++ ABLBD.m
@@ -1,39 +1,58 @@
 function [U,S,V,hist] = ABLBD(A,varargin)
+%% Modes:
+% MaxBasis, MaxMV, MaxIter, and MaxTime are always monitored
+% If nothing is set, MaxIter defaults to 20 iterations
+%
+% Currently, there are four modes that check additional criteria:
+% 1) Frobenius norm with a residual tolerance
+% 2) Residual Tolerance for k vectors
+% 3) Find all values above a global gap
+% 4) Find all values above a local gap
+%
+%
+%
+%
+%
+% The "Config" file allows for all options to be given using a *.m script
 
 p = inputParser;
 
+%% Defaults
 defaultk = inf;
 defaultBlock = 50;
 defaultAdaptive = 1e-4;
 defaultMatvecs = inf;
 defaultTime = 60;
 defaultBasis = inf;
-defaultCriteria = 9; %Time and frobenius norm
+defaultIter = inf;
 defaultDelta = 0.9;
 defaultAlpha = 0.01;
 defaultTol = 1e-3;
 defaultConfig = [];
+defaultMode = 5;
 
+%% Input checking functions
 checkMatrix = @(x) issparse(x) || ismatrix(x);
 checkInt = @(x) mod(x,1) == 0;
 checkBool = @(x) x == 0 || x == 1;
 checkConfig = @(file) ~ischar(file) && ~isStringScalar(file);
 
+%% Parser Parameters
 addRequired(p,'A',checkMatrix);
-addOptional(p,'k',defaultk,checkInt);
+addOptional(p,'Mode',defaultMode,checkInt);
+addParameter(p,'k',defaultk,checkInt);
 addParameter(p,'BlockSize',defaultBlock,checkInt);
-addParameter(p,'Adaptive',defaultAdaptive,checkBool);
+addParameter(p,'Adaptive',defaultAdaptive);
 addParameter(p,'MaxMV',defaultMatvecs,checkInt);
 addParameter(p,'MaxTime',defaultTime,@(x) x > 0);
 addParameter(p,'MaxBasis',defaultBasis,checkInt);
-addParameter(p,'Criteria',defaultCriteria,checkInt);
+addParameter(p,'MaxIter',defaultIter,checkInt);
 addParameter(p,'Delta',defaultDelta);
 addParameter(p,'Alpha',defaultAlpha);
 addParameter(p,'tol',defaultTol);
 addParameter(p,'Config',defaultConfig,checkConfig);
 
+
+%% Call Main function
 parse(p,A,varargin{:});
-
-
-
 [U,S,V,hist] = ABLBD_Main(p.Results);
--- ABLBD_Main.m
+++ ABLBD_Main.m
@@ -1,20 +1,16 @@
 function [U,S,V,hist] = ABLBD_Main(arg)
 
 A = arg.A; k = arg.k; BlS = arg.BlockSize; Adapt = arg.Adaptive;
-Criteria = uint32(arg.Criteria); delta = arg.Delta; alpha = arg.Alpha;
-MaxBasis = arg.MaxBasis; MaxMV = arg.MaxMV; MaxTime = arg.MaxTime;
-tol = arg.tol;
+delta = arg.Delta; alpha = arg.Alpha;
+MaxTime = arg.MaxTime;
+tol = arg.tol; mode = arg.Mode;
 
 if ~isempty(arg.Config)
     run(arg.Config);
 end
 
-MaxBasis = min([floor((MaxMV+BlS)/2),MaxBasis,min(size(A))]);
-assert(BlS < MaxBasis);
-
-if bitand(Criteria,32)
-    assert(k < MaxBasis); %Required for finite residual norms
-end
+[MaxMV,MaxBasis,iters] = ...
+    setComputationLength(arg.MaxMV,arg.MaxBasis,arg.MaxIter,BlS);
 
 af = norm(A,'fro');
 mvs = 0;
@@ -22,9 +18,6 @@ noexpand = 1;
 hist = {};
 rnorm = [];
 i = 1;
-conv = uint32(0);
-
-%colors = lines(iter);
 
 tic
 
@@ -36,27 +29,29 @@ end
 
 [V(:,1:BlS),~] = qr(randn(size(A,2),BlS),0);
 [U(:,1:BlS),B(1:BlS,1:BlS)] = qr(A*V(:,1:BlS),0); mvs = mvs + BlS;
+R(1:BlS,1:BlS) = B(1:BlS,1:BlS);
 [ub,sb,vb] = svd(B(1:BlS,1:BlS));
 s = diag(sb);
-%semilogy(s);
 
 prev = 0; cur = BlS; next = 2*BlS;
 
 fprintf('Time: %f\t Iter: %d\t Matvecs: %d\t k: %d\t BlS: %d\t FroNorm: %d\n',toc,i,mvs,cur,BlS,norm(s)/af);
 hist{i} = {toc, i, mvs, cur, BlS, rnorm, norm(s)/af};
 
-while ~bitand(conv,Criteria)
+while 1
+    noexpand = 1;
     i = i + 1;
-    V(:,cur+1:next) = A'*U(:,prev+1:cur) - V(:,prev+1:cur)*B(prev+1:cur,prev+1:cur);
-    mvs = mvs + BlS;
-    V(:,cur+1:next) = V(:,cur+1:next) - V(:,1:cur)*(V(:,1:cur)'*V(:,cur+1:next));
-    VtV = V(:,cur+1:next)'*V(:,cur+1:next);
-    [V(:,cur+1:next),tmp] = qr(V(:,cur+1:next),0);
-    B(prev+1:cur,cur+1:next) = tmp';
     
+    V(:,cur+1:next) = A'*U(:,prev+1:cur); mvs = mvs + BlS;
+    [V(:,cur+1:next),tmp] = cgs(V(:,1:cur),V(:,cur+1:next));
+    vr = tmp(cur+1:next,:);
+    B(prev+1:cur,cur+1:next) = vr';
+    RtR = vr'*vr;
+    
+    %multiples = max(sum(abs(s - s')./s < Adapt)); %For newer versions
+    multiples = max(sum(bsxfun(@rdivide,abs(bsxfun(@minus,s,s')),s) < Adapt));
     if Adapt ~= 0
-        multiples = max(sum(abs(s - s')./s < Adapt));
-        if multiples >= 0.9*BlS && next+BlS < MaxBasis
+        if multiples >= 0.9*BlS
             V(:,next+1:next+BlS) = cgs(V(:,1:next),randn(size(A,2),BlS));
             next = next + BlS;
             BlS = 2*BlS;
@@ -64,54 +59,109 @@ while ~bitand(conv,Criteria)
         end
     end
     
-    U(:,cur+1:next) = A*V(:,cur+1:next) - U(:,prev+1:cur)*B(prev+1:cur,cur+1:next);
-    mvs = mvs + BlS;
-    U(:,cur+1:next) = U(:,cur+1:next) - U(:,1:cur)*(U(:,1:cur)'*U(:,cur+1:next));
-    [U(:,cur+1:next),B(cur+1:next,cur+1:next)] = qr(U(:,cur+1:next),0);
+    U(:,cur+1:next) = A*V(:,cur+1:next); mvs = mvs + BlS;
+    [U(:,cur+1:next),tmp] = cgs(U(:,1:cur),U(:,cur+1:next));
+    R(1:next,cur+1:next) = tmp;
+    B(cur+1:next,cur+1:next) = tmp(cur+1:next,:);
     
-    [ub,sb,vb] = svd(B(1:next,1:next));
+    [ub,sb,vb] = svd(R(1:next,1:next));
     s = diag(sb);
-    
-    %hold on; semilogy(s,'Color',colors(floor(next/BlS),:));
-    
+
     if noexpand
-        rnorm = sqrt(diag(ub(end-BlS+1:end,:)'*VtV*ub(end-BlS+1:end,:)));
-        %    semilogy(s+run,'Color',colors(floor(next/BlS),:));
+        rnorm = sqrt(diag(ub(end-BlS+1:end,:)'*RtR*ub(end-BlS+1:end,:)));
     end
     
-    fprintf('Time: %f\t Iter: %d\t Matvecs: %d\t k: %d\t BlS: %d\t FroNorm: %d\n',toc,i,mvs,next,BlS,norm(s)/af);
-    hist{i} = {toc, i, mvs, next, BlS, rnorm, norm(s)/af};
+    fprintf('Time: %f\t Iter: %d\t Matvecs: %d\t k: %d\t BlS: %d\t FroNorm: %d\t Multiples: %d\n',toc,i,mvs,next,BlS,norm(s)/af,multiples);
+    hist{i} = {toc, i, mvs, next, BlS, s, rnorm, norm(s)/af, multiples};
     
-    if bitand(Criteria,1) && norm(s)/af > delta
-        conv = bitor(conv, 1);
+    switch mode
+        case 1  %Frobenius Norm with residual tolerance
+            if norm(s(rnorm./s(1) < tol))/af > delta
+                num_2_return = find(rnorm./s(1) < tol,1,'last');
+                break;
+            end
+        case 2  %Residual Tolerance
+            if next > k && all(rnorm(1:k) < s(1)*tol)
+                num_2_return = k;
+                break;
+            end
+        case 3 %Find all singular values above a global gap (May be too strict on residuals)
+            index = find(s < alpha*s(1),1);
+            if noexpand && s(index)+rnorm(index) < alpha*s(1) %&& ...
+                    %max(rnorm(1:ceil(index/BlS)*BlS)) < 0.1*rnorm(end)
+                %Perhaps we can only check residuals if a relaxed
+                %multiplicity count shows potential problems?...
+                num_2_return = index - 1;
+                break;
+            end
+        case 4 %Find local gap
+            gaps = 1 - (s(k+1:end)+rnorm(k+1:end))./s(1:end-k);
+            index = find(gaps > alpha);
+            if ~isempty(index)
+                num_2_return = index + k; %? not sure about this
+                break;
+            end
     end
-    if bitand(Criteria,2)
-        ind = find(rnorm+s(1:size(rnorm)) < alpha*s(1),1);
-        if rnorm(ind) < delta
-            conv = bitor(conv, 2);
+    
+    %Always check, iters, MaxBasis, MaxMV, and MaxTime
+    if i == iters || next >= MaxBasis || mvs + 2*BlS > MaxMV
+        if mode < 5
+            m1 = 'Failed to meet criteria within given number of iterations or reached maximum basis';
+            m2 = 'Increase maximum basis or iterations if returned answers are not sufficient';
+            warning('%s\n%s',m1,m2);
         end
+        num_2_return = next;
+        break;
     end
-    if bitand(Criteria,4) && mvs > MaxMV
-        conv = bitor(conv, 4);
+    if toc > MaxTime
+        num_2_return = next;
+        break;
     end
-    if bitand(Criteria,8) && toc > MaxTime
-        conv = bitor(conv, 8);
-    end
-    if bitand(Criteria,16) && next >= MaxBasis
-        conv = bitor(conv, 16);
-    end
-    if bitand(Criteria,32) && all(rnorm(1:k) < tol)
-        conv = bitor(conv, 32);
-    end
-    %Add local gap criteria
     
     prev = cur;
     cur = next;
     next = next + BlS;
 end
 
-U = U(:,1:cur)*ub(:,1:min(k,cur));
-S = sb(1:min(k,cur),1:min(k,cur));
-V = V(:,1:cur)*vb(:,1:min(k,cur));
+U = U(:,1:next)*ub(:,1:num_2_return);
+S = sb(1:num_2_return,1:num_2_return);
+V = V(:,1:next)*vb(:,1:num_2_return);
+fprintf('Time: %f\t Iter: %d\t Matvecs: %d\t k: %d\t BlS: %d\t FroNorm: %d\t Multiples: %d\n',toc,i,mvs,next,BlS,norm(s)/af,multiples);
+hist{i} = {toc, i, mvs, cur, BlS, s, rnorm, norm(s)/af, multiples};
+end
 
+function [MV,MB,I] = setComputationLength(MV,MB,I,BlS)
+config = 0;
+if I ~= inf
+config = config + 1;
+end
+if MV ~= inf
+    config = config+2;
+end
+if MB ~= inf
+    config = config+4;
+end
+
+%In general, priority goes from MB -> I -> MV
+switch config
+    case 0
+        I = 20; MV = 2*BlS*I; MB =  I*BlS;
+    case 1
+        MV = 2*BlS*I; MB =  I*BlS;
+    case 2
+        I = ceil(MV/(2*BlS)); MB = I*BlS;
+    case 4
+        I = ceil(MB/BlS); MV = 2*BlS*I;
+    case 3 %I takes priority over MV
+        MV = 2*BlS*I; MB =  I*BlS;
+    case 5 %MB takes priority over I (until restarting is added)
+        I = ceil(MB/BlS);   %Delete with restarting
+        MV = 2*BlS*I;
+    case 6 %MB takes priority over MV (until restarting is added)
+        MV = 2*MB;          %Delete with restarting
+        I = ceil(MV/(2*BlS));
+    case 7 %MB takes priority over all, iterations takes over MV
+        I = ceil(MB/BlS);   %Delete with restarting
+        MV = 2*BlS*I;
+end
 end
